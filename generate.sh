b14f312a8fffc0bbefd2f68df472b7e3f3840e1d
Manthan Dave <manthanhd@live.com>
2021-05-16 13:03:00 +0100

    Add generate.sh + rename

--- generate.sh
+++ generate.sh
@@ -1,1 +1,7 @@
+# typescript-node js docs
 openapi-generator-cli generate -g typescript-node -p "npmName=@key-connect/api,npmRepository=key-connect/sdkjs" -o . -i C:\Users\Manthan\IdeaProjects\gitlab.com\turinglabs\keyconnect\keyconnect-api\src\main\resources\api.yaml
+
+# ascii doc documentation
+openapi-generator-cli generate -g asciidoc -o . -i C:\Users\Manthan\IdeaProjects\gitlab.com\turinglabs\keyconnect\keyconnect-api\src\main\resources\api.yaml
+
+mv index.adoc readme.adoc
